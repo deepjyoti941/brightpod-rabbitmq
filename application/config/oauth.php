fc34f12928b2714f61d307db897fdada11b6084e
deepjyoti khakhlary <deepjyoti941@gmail.com>
2015-01-05 02:26:28 +0530

    new oauth key

--- application/config/oauth.php
+++ application/config/oauth.php
@@ -3,6 +3,6 @@
 }
 
 // REQUIRED VARIABLES
-$config['client_id']     = '6ca9e3bb60f9299685ec1074a19174142f150a2d';
-$config['client_secret'] = 'f85021fd9d3e90086282ce4a410ecca3589ba64c';
+$config['client_id']     = '0487906f30785e79ed68693fa1fe8eb506d0d964';
+$config['client_secret'] = '920d341cf2033ef81a5cff7007ba5e475c859ef5';
 $config['redirect_uri']  = base_url() . 'basecamp';
