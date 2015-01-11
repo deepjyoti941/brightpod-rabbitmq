85235dd81ab13b518b9e8eaf5e39380641e1c184
deepjyoti khakhlary <deepjyoti941@gmail.com>
2015-01-11 17:34:31 +0530

    gearman testing with project queue 7

--- application/controllers/cli.php
+++ application/controllers/cli.php
@@ -25,6 +25,14 @@ class Cli extends CI_Controller
         echo "Image resizing is really done.\n\n";
     }
 
+    public static function doExportProject($job) {
+        $data = unserialize($job->workload());
+        print_r($data);
+        //$this->basecamp_exporter->exportSelectedProjects($data);
+        sleep(2);
+        echo "exporting is really done.\n\n";
+    }
+
     public function client()
     {
         $this->lib_gearman->gearman_client();
@@ -37,8 +45,17 @@ class Cli extends CI_Controller
             'image' => '/var/www/pub/image/test.png',
         );
 
+        $exportData = array(
+          'user_email' => $this->input->post('user_email'),
+          'project_list' => $this->input->post('project_list')
+          );
+
+        $this->lib_gearman->do_job_background('exportProject', serialize($exportData));
+        echo "exporting project is done.\n";
+
         $this->lib_gearman->do_job_background('sendEmail', serialize($emailData));
         echo "Email sending is done.\n";
+
         $this->lib_gearman->do_job_background('resizeImage', serialize($imageData));
         echo "Image resizing is done.\n";
     }
@@ -49,6 +66,7 @@ class Cli extends CI_Controller
 
         $this->lib_gearman->add_worker_function('sendEmail', 'Cli::doSendEmail');
         $this->lib_gearman->add_worker_function('resizeImage', 'Cli::doResizeImage');
+        $this->lib_gearman->add_worker_function('exportProject', 'Cli::doExportProject');
 
         while ($this->lib_gearman->work()) {
             if (!$worker->returnCode()) {
